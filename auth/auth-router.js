8a19ef3fcc552b631aa2b2af6b7f58bdbc3a5003
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-29 12:43:21 -0500

    changes for token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +26,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+
+        const token = signToken(user) // <<<< change
+
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: ({token}), // <<<< change
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +40,20 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function signToken(user) { // <<< change, add function for token
+  const payload = {
+    userId: user.id,
+    username: user.username
+  };
+
+  const secret = 'is it secret, it is safe?'
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
